6a804c2dbd2467fe0845e3c5d465a614b2906300
Tom Zidani <tom.zidani@gmail.com>
2022-10-31 20:44:53 +0100

    Bills bug hunt fixed (proofs ext. and display).

--- front/src/containers/NewBill.js
+++ front/src/containers/NewBill.js
@@ -1,4 +1,4 @@
-import { ROUTES_PATH } from '../constants/routes.js'
+import { ROUTES_PATH } from "../constants/routes.js"
 import Logout from "./Logout.js"
 
 export default class NewBill {
@@ -15,62 +15,73 @@ export default class NewBill {
     this.billId = null
     new Logout({ document, localStorage, onNavigate })
   }
-  handleChangeFile = e => {
+  handleChangeFile = (e) => {
     e.preventDefault()
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
+    const fileName = filePath[filePath.length - 1]
+    const fileExtension = file.type
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
+    formData.append("file", file)
+    formData.append("email", email)
+
+    if (fileExtension !== "image/jpeg" && fileExtension !== "image/png") {
+      // Reset file input
+      const fileInput = this.document.querySelector(`input[data-testid="file"]`)
+      fileInput.value = ""
+
+      // and notify employee
+      return alert("Le format du justificatif est invalide (seulement .jpg, .jpeg et .png sont acceptés).")
+    }
 
     this.store
       .bills()
       .create({
         data: formData,
         headers: {
-          noContentType: true
-        }
+          noContentType: true,
+        },
       })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
+      .then(({ filePath, key }) => {
         this.billId = key
-        this.fileUrl = fileUrl
+        this.fileUrl = `http://localhost:5678/${filePath}`
         this.fileName = fileName
-      }).catch(error => console.error(error))
+      })
+      .catch((error) => console.error(error))
   }
-  handleSubmit = e => {
+  handleSubmit = (e) => {
     e.preventDefault()
     console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
     const email = JSON.parse(localStorage.getItem("user")).email
     const bill = {
       email,
       type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
-      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
+      name: e.target.querySelector(`input[data-testid="expense-name"]`).value,
       amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
-      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
+      date: e.target.querySelector(`input[data-testid="datepicker"]`).value,
       vat: e.target.querySelector(`input[data-testid="vat"]`).value,
       pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
       commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
       fileUrl: this.fileUrl,
       fileName: this.fileName,
-      status: 'pending'
+      status: "pending",
     }
+
     this.updateBill(bill)
-    this.onNavigate(ROUTES_PATH['Bills'])
+    this.onNavigate(ROUTES_PATH["Bills"])
   }
 
   // not need to cover this function by tests
   updateBill = (bill) => {
     if (this.store) {
       this.store
-      .bills()
-      .update({data: JSON.stringify(bill), selector: this.billId})
-      .then(() => {
-        this.onNavigate(ROUTES_PATH['Bills'])
-      })
-      .catch(error => console.error(error))
+        .bills()
+        .update({ data: JSON.stringify(bill), selector: this.billId })
+        .then(() => {
+          this.onNavigate(ROUTES_PATH["Bills"])
+        })
+        .catch((error) => console.error(error))
     }
   }
 }
